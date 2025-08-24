21f62f8acdf3e8eaab843f164efddcbaac9b4872
vuongskysurferstorage <vuongskysurferstorage1@gmail.com>
2025-08-23 23:47:14 -0400

    360 Images Upload

---
+++ data.js
@@ -0,0 +1,221 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-harrisburg-first-assembly-of-god-church-harrisburg-pa",
+      "name": "Harrisburg First Assembly of God Church Harrisburg, PA",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3000,
+      "initialViewParameters": {
+        "yaw": 0.03442210529999201,
+        "pitch": 0,
+        "fov": 1.4134061960355204
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.0021691496822651146,
+          "pitch": -0.8957735666731246,
+          "rotation": 0,
+          "target": "1-harrisburg-first-assembly-of-god-church-harrisburg-pa"
+        },
+        {
+          "yaw": 3.1173355753420555,
+          "pitch": -0.3947886657587425,
+          "rotation": 0,
+          "target": "2-harrisburg-first-assembly-of-god-church-harrisburg-pa"
+        },
+        {
+          "yaw": -0.0006834269350814992,
+          "pitch": -0.056269071404564386,
+          "rotation": 0,
+          "target": "3-harrisburg-first-assembly-of-god-church-harrisburg-pa"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-harrisburg-first-assembly-of-god-church-harrisburg-pa",
+      "name": "Harrisburg First Assembly of God Church Harrisburg, PA",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.1207940237205047,
+          "pitch": 0.846947270145531,
+          "rotation": 0,
+          "target": "0-harrisburg-first-assembly-of-god-church-harrisburg-pa"
+        },
+        {
+          "yaw": 3.1293164632629287,
+          "pitch": -0.09422175441112302,
+          "rotation": 0,
+          "target": "2-harrisburg-first-assembly-of-god-church-harrisburg-pa"
+        },
+        {
+          "yaw": 0.00451116940303109,
+          "pitch": 0.1370184679379456,
+          "rotation": 0,
+          "target": "3-harrisburg-first-assembly-of-god-church-harrisburg-pa"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-harrisburg-first-assembly-of-god-church-harrisburg-pa",
+      "name": "Harrisburg First Assembly of God Church Harrisburg, PA",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.014017825061944933,
+          "pitch": -0.038732697720725184,
+          "rotation": 0,
+          "target": "1-harrisburg-first-assembly-of-god-church-harrisburg-pa"
+        },
+        {
+          "yaw": -0.009428093934332082,
+          "pitch": 0.695650990585996,
+          "rotation": 0,
+          "target": "0-harrisburg-first-assembly-of-god-church-harrisburg-pa"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-harrisburg-first-assembly-of-god-church-harrisburg-pa",
+      "name": "Harrisburg First Assembly of God Church Harrisburg, PA",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.003939851139451278,
+          "pitch": -0.11936175138370864,
+          "rotation": 0,
+          "target": "2-harrisburg-first-assembly-of-god-church-harrisburg-pa"
+        },
+        {
+          "yaw": -0.0018179237826547734,
+          "pitch": 0.2666085130655773,
+          "rotation": 0,
+          "target": "0-harrisburg-first-assembly-of-god-church-harrisburg-pa"
+        },
+        {
+          "yaw": -0.011856609042080635,
+          "pitch": -0.5781500115809202,
+          "rotation": 0,
+          "target": "1-harrisburg-first-assembly-of-god-church-harrisburg-pa"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
